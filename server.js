b64ae13474fa933cbda01f0c15c990f5d5d1fe21
kassidyr <kassidy.ralph@gmail.com>
2021-12-10 12:43:38 -0500

    add database query

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '2@Zeppelin2@',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found); should be last route in file
 app.use((req, res) => {
